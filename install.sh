cdb3c2680c8ff5b66bda22fd8bee95ec10b6d44e
Karol Moryc <karol.moryc@volvocars.com>
2024-10-14 21:21:48 +0200

    Add onedark color theme

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 function install_python_ubuntu() {
   sudo apt install python3-pip python3-venv ctags
@@ -14,11 +14,22 @@ function install_vim() {
 
   _vundle_dir=~/.vim/bundle/Vundle.vim
   _vundle_url=https://github.com/VundleVim/Vundle.vim.git
-  if ! git clone $_vundle_url $_vundle_dir 2>/dev/null && [ -d $_vundle_dir ]; then
+  if ! git clone $_vundle_url $_vundle_dir 2>&1 && [ -d $_vundle_dir ]; then
     echo "Directory $_vundle_dir already exists. Skipping installation of: VundleVim"
   fi
-  
-  vim +slient +VimEnter +PluginInstall +qall
+ 
+  _onedark_dir=~/.vim/bundle/onedark.vim
+  _onedark_url=https://github.com/joshdick/onedark.vim.git
+  if ! git clone $_onedark_url $_onedark_dir 2>&1 && [ -d $_onedark_dir ]; then
+    echo "Directory $_onedark_dir already exists. Skipping installation of: onedark.vim"
+  else
+    mkdir -p ~/.vim/colors
+    cp $_onedark_dir/colors/onedark.vim ~/.vim/colors/onedark.vim  
+    mkdir -p ~/.vim/autoload
+    cp $_onedark_dir/autoload/onedark.vim ~/.vim/autoload/onedark.vim 
+  fi
+
+  vim +PluginInstall +qall
 }
 
 JOB=${1:-ubuntu}
